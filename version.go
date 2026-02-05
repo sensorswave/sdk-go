70ed9cd92a5f8a54b75bddb12cbc728834708466
yanming <yanming@sensorsdata.cn>
2026-02-05 20:13:06 +0800

    chore: bump version to 0.1.2

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package sensorswave
 
 // version is the SDK version, injected at build time via ldflags.
 // Default value is "dev" for local development.
-var version = "0.1.1"
+var version = "0.1.2"
 
 // sdkType identifies this SDK implementation.
 const sdkType = "go"
