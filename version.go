9c7effa0c0399e339756063497de5d7d4ff9a563
yanming <yanming@sensorsdata.cn>
2026-05-21 19:27:51 +0800

    chore: update to 0.1.8

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package sensorswave
 
 // version is the SDK version, injected at build time via ldflags.
 // Default value is "dev" for local development.
-var version = "0.1.7"
+var version = "0.1.8"
 
 // sdkType identifies this SDK implementation.
 const sdkType = "go"
